51b0269dbce3bc83a8241bf560951caa737d5b9b
VforWedth <lorendrain47@gmail.com>
2024-12-14 12:47:19 +0630

    Initial Commit

---
+++ Phone_Moe_Htet/main.lua
@@ -0,0 +1,14 @@
+---@diagnostic disable: 11
+function love.load()
+    
+end
+
+function love.update(dt)
+    -- statements
+end
+
+---@diagnostic disable-next-line: unused-vararg
+function love.draw( ... )
+    -- body
+    love.graphics.print("Hi")
+end
